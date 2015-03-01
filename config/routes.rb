c5ce4b2475fc76d5204769f46cf094273e81be39
Vinisha Hathiramani <vinishash@gmail.com>
2015-03-01 18:31:05 +0000

    routes. welcome controller. controller for events. partial form for events. edit page for events

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcomes#index'
 
+  resources :events
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
