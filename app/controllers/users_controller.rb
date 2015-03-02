78ef9cbf596ab8d6d9a36f8547c1fdd5b067fee6
Natalie Loh <Natalie@Natalies-MacBook-Pro.local>
2015-03-02 17:09:35 +0000

    ajax button for matches still wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
   def user_params
     params.require(:user).permit(:id, :dog_name, :match_list => [])
   end
