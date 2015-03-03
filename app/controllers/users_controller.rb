2140101b64c140546aab67a9efdc502aec938e6c
Vinisha Hathiramani <vinishash@gmail.com>
2015-03-03 17:49:33 +0000

    trying to merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def update_preferences
     preferences = params[:user]
     current_user.preference_list.add(preferences)
-    binding.pry
+    # binding.pry
   end
 
 
