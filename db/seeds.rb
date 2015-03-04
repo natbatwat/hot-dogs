701b4fd2d002a572f9ffd90a6f98fb4632bb6a14
Natalie Loh <Natalie@Natalies-MacBook-Pro.local>
2015-03-04 11:22:28 +0000

    removed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,3 @@ Review.delete_all
 
 # SEED DATA FOR ASSOCIATIONS TESTING #
 # USERS #
-u1 = User.create(
-  email:"bob@email.com",
-  encrypted_password: "password",
-  dog_name: "bax",
-  owner_name: "bob",
-  )
-
-u2 = User.create(
-  email:"sally@email.com",
-  encrypted_password: "password",
-  dog_name: "ted",
-  owner_name: "sally",
-  )
-
-p1 = Playdate.create(
-  name:"date at bax house",
-  )
-
-ev1 = Event.create(name:'Doggy Brunch', place:'Gaucho, Hampstead')
-ev2 = Event.create(name:'Doggy Walks', place:'Regents Park')
-ev1 = Event.create(name:'Doggy Speed Dates', place:'The Duke of York, St Johns Wood')
