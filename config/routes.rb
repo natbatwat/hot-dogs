fec97488e8ba1fbb4cc1851070a5358a1c41c04b
Natalie Loh <Natalie@Natalies-MacBook-Pro.local>
2015-03-04 17:00:34 +0000

    resolve routing issues

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: { :sessions => "users/sessions", :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations"}
 
-  devise_scope :user do
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
-
   resources :users do
     resources :reviews
   end
