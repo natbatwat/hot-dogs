26cfccd06bf141757e6883e8064f87398fde6a71
Vinisha Hathiramani <vinishash@gmail.com>
2015-03-05 14:36:24 +0000

    MAP WORKING GET IN

--- app/assets/javascripts/playdates.js
+++ app/assets/javascripts/playdates.js
@@ -1,35 +1,74 @@
 if ($('map-canvas')){
-  var geocoder;
-  var map;
-  function initialize() {
-    
-
-    geocoder = new google.maps.Geocoder();
-
-    var latlng = new google.maps.LatLng(51.519889, -0.068799);
+  function initialize(){
+    var myLatLng = new google.maps.LatLng(51.5361, -0.1751)
+    var mapCanvas = document.getElementById('map-canvas');
     var mapOptions = {
+      center: myLatLng,
       zoom: 16,
-      center: latlng
+      mapTypeId: google.maps.MapTypeId.ROADMAP
     }
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    codeAddress();
-  }
-
-  function codeAddress() {
-    var address = $('#address').text();
-    console.log(address)
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    addMarker(map);
+  
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
     });
-  }
+}
+    function addMarker(map) {
 
+     if(navigator.geolocation){
+       navigator.geolocation.getCurrentPosition(function(position){
+         var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+         var infowindow = new google.maps.InfoWindow({
+           map: map,
+           position: pos,
+           content: "Location set using HTML5"
+         });
+
+         map.setCenter(pos);
+       }, function() {
+         handleNoGeolocation(true);
+       });
+     } else {
+       handleNoGeolocation(false);
+     }
+   }
+
+   function handleNoGeolocation(errorFlag) {
+     if (errorFlag) {
+       var content = 'error: geolocation server failed';
+     } else {
+       var content = 'error: your browser does not support geolocation';
+     }
+
+   var infowindow = new google.maps.InfoWindow(options);
+   map.setCenter(options.position);
+
+     }
   google.maps.event.addDomListener(window, 'load', initialize);
 }
+
+//     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+//     codeAddress();
+//   }
+
+//   function codeAddress() {
+//     var address = $('#address').text();
+//     console.log(address)
+//     geocoder.geocode( { 'address': address}, function(results, status) {
+//       if (status == google.maps.GeocoderStatus.OK) {
+//         map.setCenter(results[0].geometry.location);
+//         var marker = new google.maps.Marker({
+//             map: map,
+//             position: results[0].geometry.location
+//         });
+//       } else {
+//         alert('Geocode was not successful for the following reason: ' + status);
+//       }
+//     });
+//   }
+
+//   google.maps.event.addDomListener(window, 'load', initialize);
+// }
